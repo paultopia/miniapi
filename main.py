7107b2df5dba57d82960cc64e7509eaca50e5270
Paul Gowder <paul.gowder@gmail.com>
2018-06-26 03:26:57 +0000

    pandoc

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import pypandoc
+
 app = Flask(__name__)
+
+testmd = '''
+# Hello Paul!
+
+You should check out [your own webpage](https://gowder.io)!
+'''
+
+testhtml = pypandoc.convert_text(testmd, 'html', format='md')
+
 
 @app.route("/")
 def hello():
-    return "Hello miniapi"
+    return pypandoc.convert_file("test.md", "html")
 
 if __name__ == "__main__":
     app.run()
---
+++ test.md
@@ -0,0 +1,5 @@
+# Hello Paul!
+
+You should check out [your own webpage](https://gowder.io)!
+
+Riiight?
