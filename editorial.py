d173b39dd10b607f84d5b8115cfcfd139c40d3b2
Paul Gowder <paultopia@users.noreply.github.com>
2018-06-25 22:33:58 -0700

    fix other bugs

--- editorial.py
+++ editorial.py
@@ -3,9 +3,9 @@
 # this file is for the editorial app (ios) workflow to send a markdown here to convert to pdf. 
 import editor, requests, workflow
 
-IP_ADDRESS = "secret"  # yeah this is crappy security but who cares? worst case scenario you HAXOR MY PANDOC INSTALLATION 1337 PWNAGE!!!
+IP_ADDRESS = "SECRET"  # yeah this is crappy security but who cares? worst case scenario you HAXOR MY PANDOC INSTALLATION 1337 PWNAGE!!!
 
-target= "http://" + IP_ADDRESS + ":8080/md2pdf"
+target= "http://" + IP_ADDRESS + ":8080/mdpdf"
 
 # this comes from workflow actions "get current file name" and then "set variable" (Input to "filename")
 filename = workflow.get_variable("filename") + ".pdf"
@@ -13,9 +13,8 @@ filename = workflow.get_variable("filename") + ".pdf"
 markdown = editor.get_text(True)
 
 data = {"filename": filename, "markdown": markdown}
-
-response = post(path, headers=headers, data=data)
-pdf = requests.post(target, data=data).content
+response = requests.post(target, data=data)
+pdf = response.content
 editor.set_file_contents(filename, pdf, 'dropbox')
 
 print('done!')
