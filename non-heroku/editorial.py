050d61785fad56652c9c3a69731fdf09f63a7bbc
Paul Gowder <paul.gowder@gmail.com>
2018-11-03 13:57:24 -0500

    pandoc works

--- non-heroku/editorial.py
+++ non-heroku/editorial.py
@@ -3,9 +3,9 @@
 # this file is for the editorial app (ios) workflow to send a markdown here to convert to pdf. 
 import editor, requests, workflow
 
-IP_ADDRESS = "SECRET"  # yeah this is crappy security but who cares? worst case scenario you HAXOR MY PANDOC INSTALLATION 1337 PWNAGE!!!
+HEROKU = "SECRET"  # yeah this is crappy security but who cares? worst case scenario you HAXOR MY PANDOC INSTALLATION 1337 PWNAGE!!!
 
-target= "http://" + IP_ADDRESS + ":8080/mdpdf"
+target= "https://" + HEROKU + "/mdpdf"
 
 # this comes from workflow actions "get current file name" and then "set variable" (Input to "filename")
 filename = workflow.get_variable("filename") + ".pdf"
