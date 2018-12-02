21c412ca6ef0eaf3f9f0f79f2674a9fea746b6af
Paul Gowder <paul.gowder@gmail.com>
2018-12-02 14:35:39 -0600

    xelatex

--- webapp/conversions.py
+++ webapp/conversions.py
@@ -1,12 +1,16 @@
 import pypandoc
 from filehandling import extension
 
+def addargs(output_format):
+    if output_format == "pdf":
+        return ["--pdf-engine=xelatex"]
+    return []
 
 def base_conversion(content, output_format, input_format, outfile):
     if output_format in ["odt", "docx", "epub", "epub3", "pdf"]:
-        pypandoc.convert_text(content, output_format, format=input_format, outputfile=outfile)
+        pypandoc.convert_text(content, output_format, format=input_format, outputfile=outfile, extra_args = addargs(output_format))
         return {"is_file": True, "content": None, "filename": outfile}
-    outcontent = pypandoc.convert_text(content, output_format, format=input_format)
+    outcontent = pypandoc.convert_text(content, output_format, format=input_format, extra_args = addargs(output_format))
     return {"is_file": False, "content": outcontent, "filename": None}
 
 
